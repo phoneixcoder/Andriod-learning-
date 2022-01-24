9d7239614fac99ba1f511b1659cb9c6649cb8d6e
phoneixcoder <atritilak486@gmail.com>
2022-01-25 00:48:19 +0530

    Downloading Image

---
+++ DownloadingImages/app/src/main/java/com/example/downloadingimages/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.downloadingimages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    public void downloadImage(View view){
+        imageDownloader task = new imageDownloader();
+        Bitmap myImage;
+        try {
+            myImage = task.execute("https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png").get();
+            imageView.setImageBitmap(myImage);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+    }
+    public class imageDownloader extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream in = connection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(in);
+
+                return bitmap;
+            }catch(Exception e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+}
